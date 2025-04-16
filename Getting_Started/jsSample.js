ce13660a89593161c630ea474604ee42063ef148
sfdjraj <sfdjraj@gmail.com>
2025-04-16 10:18:44 -0700

    first commit

--- Getting_Started/jsSample.js
+++ Getting_Started/jsSample.js
@@ -2,4 +2,5 @@ console.log ("----------------------------------");
 console.log ("----------Rise and Shie!----------");
 console.log ("-------Ready For A New Day Again--------");
 console.log ("---- Lets do it again--------");
+console. log ("..TEST..");
 console.log ("----------------------------------");
